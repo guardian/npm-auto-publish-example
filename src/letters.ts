c11248a57a75f12d2ae280ffce911f642a6b1b9a
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-03-18 13:55:38 +0000

    fix: add n (#99)

--- src/letters.ts
+++ src/letters.ts
@@ -1,1 +1,1 @@
-export const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"];
+export const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"];
