7674562b8eb0d65ad42a603461e103de533309f2
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-03-23 09:38:56 +0000

    fix: add q

--- src/letters.ts
+++ src/letters.ts
@@ -1,1 +1,1 @@
-export const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"];
+export const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q"];
